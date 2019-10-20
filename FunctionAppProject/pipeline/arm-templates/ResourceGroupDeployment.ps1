e024e6c1813eb9132eb291f79f9cb8480996852b
Balázs Keresztúri <19balazs86@gmail.com>
2019-10-20 19:54:43 +0100

    Add ARM template files.

---
+++ FunctionAppProject/pipeline/arm-templates/ResourceGroupDeployment.ps1
@@ -0,0 +1,25 @@
+﻿param(
+  [string]
+  [Parameter(Mandatory=$true)]
+  $deploymentName,
+
+  [string]
+  [Parameter(Mandatory=$true)]
+  $resourceGroup,
+
+  [string]
+  [Parameter(Mandatory=$true)]
+  $templateFile,
+
+  [string]
+  [Parameter(Mandatory=$true)]
+  $templateParameterFile
+)
+
+# https://docs.microsoft.com/en-us/powershell/module/az.resources/new-azresourcegroupdeployment
+
+New-AzResourceGroupDeployment `
+  -Name $deploymentName `
+  -ResourceGroupName $resourceGroup `
+  -TemplateFile $templateFile `
+  -TemplateParameterFile $templateParameterFile
