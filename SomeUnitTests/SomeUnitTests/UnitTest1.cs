2feeee6d9e8ba248bafead62fe929dae66fa0ba0
Balázs Keresztúri <19balazs86@gmail.com>
2019-10-23 21:08:51 +0100

    Add unit test project

---
+++ SomeUnitTests/SomeUnitTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+using Xunit;
+
+namespace SomeUnitTests
+{
+  public class UnitTest1
+  {
+    [Fact(DisplayName = "Test something #1")]
+    public void Test1()
+    {
+
+    }
+
+    [Fact]
+    public void Test2()
+    {
+
+    }
+
+    [Fact]
+    public void Test3()
+    {
+
+    }
+  }
+}
